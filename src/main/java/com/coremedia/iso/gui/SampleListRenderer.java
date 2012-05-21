a19faf716326e921ff5d6b62ade3a41ff6d74f1d
michael.stattmann@gmail.com <michael.stattmann@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2012-05-21 11:49:32 +0000

    show NAL type and idc
    git-svn-id: http://mp4parser.googlecode.com/svn/trunk/tools/isoviewer@621 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/coremedia/iso/gui/SampleListRenderer.java
+++ src/main/java/com/coremedia/iso/gui/SampleListRenderer.java
@@ -1,6 +1,8 @@
 package com.coremedia.iso.gui;
 
 import com.coremedia.iso.boxes.h264.AvcConfigurationBox;
+import com.coremedia.iso.boxes.sampleentry.SampleEntry;
+import com.coremedia.iso.boxes.sampleentry.VisualSampleEntry;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
@@ -22,19 +24,20 @@ public class SampleListRenderer extends DefaultListCellRenderer {
         SampleListModel.Entry sampleListEntry = (SampleListModel.Entry) value;
 
         value = "Sample " + (index + 1) + "@" + sampleListEntry.offset + " - " + sampleListEntry.sample.limit() + "bytes";
-        if (sampleListEntry.se != null &&
-                ("avc1".equals(sampleListEntry.se.getType()) || "mp4v".equals(sampleListEntry.se.getType()))) {
+        final SampleEntry se = sampleListEntry.se;
+        if (se != null && se instanceof VisualSampleEntry &&
+                ("avc1".equals(se.getType()) || "mp4v".equals(se.getType()) || ("encv".equals(se.getType()) && ((VisualSampleEntry) se).getCompressorname().contains("AVC")))) {
             try {
                 IsoSampleNALUnitReader isoSampleNALUnitReader = new IsoSampleNALUnitReader(sampleListEntry.sample,
-                        sampleListEntry.se.getBoxes(AvcConfigurationBox.class).get(0).getLengthSizeMinusOne() + 1);
-                ArrayList<ByteBuffer> nals = new ArrayList<ByteBuffer>();
+                        se.getBoxes(AvcConfigurationBox.class).get(0).getLengthSizeMinusOne() + 1);
+                ArrayList<NalWrapper> nals = new ArrayList<NalWrapper>();
 
                 do {
                     ByteBuffer nal = isoSampleNALUnitReader.nextNALUnit();
                     if (nal == null) {
                         break;
                     }
-                    nals.add(nal);
+                    nals.add(new NalWrapper(nal));
                 } while (true);
 
                 value = "Sample " + (index + 1) + "@" + sampleListEntry.offset + " - " + sampleListEntry.sample.limit() + "bytes " + nals;
@@ -44,5 +47,31 @@ public class SampleListRenderer extends DefaultListCellRenderer {
         }
         super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         return this;
+    }
+
+    public class NalWrapper {
+        ByteBuffer data;
+
+        public NalWrapper(ByteBuffer data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append("NalWrapper");
+            sb.append("{").append(printNal(data));
+            sb.append('}');
+//            sb.append("{data=").append(data);
+//            sb.append('}');
+            return sb.toString();
+        }
+    }
+    public String printNal(ByteBuffer nal) {
+        int type = nal.get();
+        int nal_ref_idc = (type >> 5) & 3;
+        int nal_unit_type = type & 0x1f;
+        return "Type: " + nal_unit_type + " ref_idc: " + nal_ref_idc + " (size " + nal.limit() + 1 + ")";
+
     }
 }
